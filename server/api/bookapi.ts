a09ab8ca7810a5b54551d766550f5acdeab8f280
greeng00se <thegreengoose7@gmail.com>
2022-06-26 21:10:39 +0900

    first commit

---
+++ server/api/bookapi.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import type { IncomingMessage, ServerResponse } from "http";
+import * as url from "url";
+
+export default async (req: IncomingMessage, res: ServerResponse) => {
+    const query = url.parse(req.url as string, true).query;
+    let { bookQuery } = query;
+    let responseData = {};
+    
+    if (bookQuery) {
+        responseData = await axios.get(`http://localhost:3000/books?bookQuery=${bookQuery}`);
+    }
+
+    res.writeHead(200, { "Content-Type": "application/json"});
+    res.write(JSON.stringify(responseData.data));
+    res.end();
+}
