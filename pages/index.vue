1ae07dfca03960679b4b8b29e9faee91d6a97ae9
greeng00se <thegreengoose7@gmail.com>
2022-07-07 13:53:34 +0900

    index 샘플 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="d-flex align-center flex-column">
-    <v-icon large color="green darken-2">
-      mdi-domain
-    </v-icon>
-    <v-card width="400">
-      <v-card-header>
-        <v-card-header-text>
-          <v-card-title>This is a title</v-card-title>
-          <v-card-subtitle>This is a subtitle</v-card-subtitle>
-        </v-card-header-text>
-      </v-card-header>
-      <v-card-text> This is content </v-card-text>
-    </v-card>
+    <v-icon large color="green darken-2">mdi-domain</v-icon>
+    <v-btn
+      color="primary"
+      elevation="2"
+      outlined
+      rounded
+    >Click</v-btn>
   </div>
 </template>
